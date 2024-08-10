a04b0c0f9c7c31f71e75152d0dcdbf70bf08f8a5
Daniela Cardozo <danielacar89@hotmail.com>
2024-08-10 17:33:42 -0300

    Hola!soyeldiv-final

--- eventos.js
+++ eventos.js
@@ -3,8 +3,3 @@ const miboton = document.getElementById('boton')
 midiv.addEventListener('click', function() {
     alert('¡Hola! Soy el div');
 });
-
-miboton.addEventListener('click', function(evento) {
-    evento.stopPropagation();
-});
-//Solucion-click arriba
